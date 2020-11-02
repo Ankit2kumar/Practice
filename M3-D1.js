7832dbb9ecdba65631892faad0c66b2bd6bd4a7c
Ankit2kumar <50981000+Ankit2kumar@users.noreply.github.com>
2020-11-02 17:31:36 +0100

    Add files via upload
    Completed till 13

--- M3-D1.js
+++ M3-D1.js
@@ -1,160 +1,207 @@
 /*
-
 1)
 Create a function to calculate the sum of the two given integers. If the two values are same, then returns triple their sum.
 */
-const calculate= function(a,b){
-    if(a===b){
-        return (3*(a+a))
+const sumFunction = function(num1,num2){
+    if(num1!=num2){
+        return num1+num2
     }else{
-        return a+b
+        return 3 * (num1 + num2)
     }
 }
-//console.log(calculate(6,6));
-
-
+//console.log(sumFunction(5,5))
 
 /*
 2)
 Create a function to check two given numbers and return true if one of the number is 50 or if their sum is 50.
 */
-const check=function(a,b){
-if(a+b===50 ||a==50 || b==50){
-    return true
-}else{
-    return false
+const check50 = function(num1, num2){
+    if(num1 ===50 || num2 === 50 || (num1+num2) === 50){
+        return true
+    }else{
+        return false
+    }
 }
-}
-  //console.log(check(25,25));  
+//console.log(check50(50,25))
+
+
 /*
 3)
 Create a function to remove a character at the specified position of a given string and return the new string.
 */
-    const remove=function(str,pos){
-       return (str.slice(0,pos)+str.slice(pos+1,str.length));
-    }
-   // console.log(remove('NOVEMBER', 3))
+const removeCharacter = function(str,pos){
+   
+    return (str.slice(0,pos) + str.slice(pos+1, str.length));
+
+}
+//console.log(removeCharacter("November", 4))
+
+
 /*
 
 4)
  Create a function to find the largest of three given integers.
 */
-    const largest= function(a,b,c){
-        if(a>b && a>c)
-        return a
-        else if(b>a && b>c)
-        return b
+
+const largest = function(x,y,z){
+        if(x>y && x>z)
+            return x
+        else if(y>x && y>z)
+            return y
         else
-        return c
-    }   
-    //console.log(largest(3,2,1)); 
+            return z
     
+}
+//console.log(largest(5,0,0))
 /*
 5)
 Create a function to check whether two numbers are in range 40..60 or in the range 70..100 inclusive.
 */
-    const range=function(x,y){
-    if(x>=40 && x<=60 || x>=70 && x<=100) 
-        return x
-        else if (y>=40 && y<=60 || y>=70 && y<=100)
-        return y
-        else
-        return false
-    }
-   // console.log(range(100,10))
+const checkRange = function(num1, num2){
+if ((num1 && num2) >= 40 && (num1 && num2) <=60 || (num1 && num2) >= 70 && (num1 && num2) <=100)
+return true
+else
+return false
+}
+//console.log(checkRange(10,10))
+
 /*
 6) 
 
 Create a function to create a new string of specified copies (positive number) of a given string.
 */
-       
+const copiesString = function(str, cop){
+    let newStrArr = [];
+    let res = " ";
+    for (let i =0; i<cop; i++){
+    newStrArr[i] = str;
+     res=  res + newStrArr[i]
+    
+        }
+            return res
+}
+//console.log(copiesString("Ankitwa",5))
 /*
+
 
 7)
 Create a function to display the city name if the string begins with "Los" or "New" otherwise return blank.
 */
-            const name=function(a){
-                if(a.includes('Los' || 'New')){
-                    return true
-                }else{
-                    return false
-                }
-            }
-          //  console.log(name('Los Angeles'));
+const displayCity = function(str){
+    for (let i=0; i<=2; i++){
+        if(str[i]==='N' && str[i+1]==='e' &&  str[i+2]==='w'){
+            let res = "New York"
+            return res
+            break 
+        }else if (str[i]==='L' && str[i+1]==='o' &&  str[i+2]==='s') {
+            let res = "Los Angeles"
+            return res
+            break
+        } else{
+            return false
+        }
+    }
+}
+//console.log(displayCity("NewApple"))
 /*
 8)
 Create a function to calculate the sum of three elements of a given array of integers of length 3.
 */
-            const calculateNow=function(arr){
-                let sum=0
-                for(let i=0;i<arr.length;i++){
-                    sum= sum+arr[i]
-                }
-                return sum
-            }
-            //console.log(calculateNow([3,4,6]));
+
+const calSum = function( arr ){
+    let finSum = 0;
+ for ( let i =0; i<arr.length; i++){
+    finSum = finSum + arr[i]
+ }
+ return finSum
+}
+//console.log(calSum([1,2,3,0,2,2,2,2,2,2,2,2,2,2,22,2,2,0]))
 /*
 
 9)
 Create a function to test whether an array of integers of length 2 contains 1 or a 3. 
 */
-            const test=function(arr2){
-                if(arr2.includes(1 || 3))
-                return true
-                else 
-                return false
-            }
-           // console.log(test([2,4,5,6,7,8,9,0]));
-            
+const testArray = function(arr){
+    for (let i=0; i<arr.length; i++){
+        if((arr[i] === 1) || (arr[i] === 3))
+        return true
+        else
+        return false
+    }
+}
+//console.log(testArray([1,2,2,2,2,2,1,1,2,1,2,1,2,1,2,1,,1,2,1,2,1,2,1,1,2,1,2,1,1,1,2,1,,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1]))
 /*
 
 10)
 
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3 */
 
-        const test2=function(arr2){
-                if(arr2.includes(1 || 3))
-                return true
-                else 
-                return false
-            }   
-
-
-            /*  
+const testArray1 = function(arr){
+    for (let i=0; i<arr.length; i++){
+    
+        if((arr[i] != 1) || (arr[i] != 3)){
+        let test = true
+        return test
+        }
+        else
+        return false
+    }
+}
+//console.log(testArray1([1,2,3,4]))
+ /*
 11)
 
-Create a function to find the longest string from a given array of strings. */
-            const arrString=function(){
+Create a function to find the longest string from a given array of strings.
+**/
 
-            }
+const longestArr = function(arrStr){
 
+let lgth = 0;
+let longest;
+
+for(let i=0; i < arrStr.length; i++){
+    if(arrStr[i].length > lgth){
+        let lgth = arrStr[i].length;
+        longest = arrStr[i];
+    } else{
+        continue
+    }     
+}
+return longest 
+
+}
+console.log (longestArr(["abc", "hjasdads", "aksdjad", "jaasb"]));
 
 /*
+ 
+12)
+
 Create a function to find the types of a given angle.
 
 Types of angles:
     Acute angle: An angle between 0 and 90 degrees.
     Right angle: An 90 degree angle.
     btuse angle: An angle between 90 and 180 degrees.
-    Straight angle: A 180 degree angle.*/
-      
-    const angle= function(a){
-        if(a>=0 && a<=90){
-            return acuteAngle
-        }else if(a===90)
-        return rightAngle
-        else if(a>=91 && a<=180){
-            return obtuseAngle
-        }else 
-        return straightLine
-    }
-    console.log(angle(35));
+    Straight angle: A 180 degree angle. */
 
 
- /*   
+    /*
 13)
 
-Create a function to find the index of the greatest element of a given array of integers
+Create a function to find the index of the greatest element of a given array of integers */
+ 
+const greatest = function(arrInt){
+    let greatest = 0;
+for(let i=0; i<arrInt.length; i++){
+ if(arrInt[i]> greatest){
+     r
+ }
+}
+return greatest
+}
+
+console.log(greatest([1,2,3,4,5,6,7,1,2,10,100,1,1,1,0]))
+/*
 
 14)
 
